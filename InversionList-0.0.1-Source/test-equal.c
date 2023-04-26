fb1a155458e2924fdccfe7f50e6c452c0a0e2483
Hugo <>
2023-04-26 11:04:41 +0200

    Add new tests

--- InversionList-0.0.1-Source/test-equal.c
+++ InversionList-0.0.1-Source/test-equal.c
@@ -29,6 +29,106 @@ int main(void) {
         inversion_list_destroy(set2);
         inversion_list_destroy(set_clone_cap);
     }
+    {
+        unsigned int a[] = {5, 6, 7, 78, 79, 80, 81, 90};
+        unsigned int b[] = {5, 6, 7, 78, 79, 80, 81};
+        unsigned int c[] = {6, 7, 78, 79, 80, 81, 90};
+        unsigned int d[] = {5, 6, 7, 78, 79, 80, 81, 90, 50};
+
+        InversionList *set_a_uint8 = inversion_list_create(100, sizeof a / sizeof *a, a);
+        InversionList *set_a_uint16 = inversion_list_create(1000, sizeof a / sizeof *a, a);
+        InversionList *set_a_uint32 = inversion_list_create(100000, sizeof a / sizeof *a, a);
+        InversionList *set_a_bis_uint8 = inversion_list_create(100, sizeof a / sizeof *a, a);
+        InversionList *set_a_bis_uint16 = inversion_list_create(1000, sizeof a / sizeof *a, a);
+        InversionList *set_a_bis_uint32 = inversion_list_create(100000, sizeof a / sizeof *a, a);
+
+        InversionList *set_b_uint8 = inversion_list_create(100, sizeof b / sizeof *b, b);
+        InversionList *set_b_uint16 = inversion_list_create(1000, sizeof b / sizeof *b, b);
+        InversionList *set_b_uint32 = inversion_list_create(100000, sizeof b / sizeof *b, b);
+
+        InversionList *set_c = inversion_list_create(100, sizeof c / sizeof *c, c);
+        InversionList *set_d = inversion_list_create(100, sizeof d / sizeof *d, d);
+
+        assert(inversion_list_equal(set_a_uint8, set_a_uint8));
+        assert(inversion_list_equal(set_a_uint8, set_a_uint16));
+        assert(inversion_list_equal(set_a_uint8, set_a_uint32));
+        assert(inversion_list_equal(set_a_uint16, set_a_uint8));
+        assert(inversion_list_equal(set_a_uint16, set_a_uint16));
+        assert(inversion_list_equal(set_a_uint16, set_a_uint32));
+        assert(inversion_list_equal(set_a_uint32, set_a_uint8));
+        assert(inversion_list_equal(set_a_uint32, set_a_uint16));
+        assert(inversion_list_equal(set_a_uint32, set_a_uint32));
+        assert(!inversion_list_not_equal(set_a_uint8, set_a_uint8));
+        assert(!inversion_list_not_equal(set_a_uint8, set_a_uint16));
+        assert(!inversion_list_not_equal(set_a_uint8, set_a_uint32));
+        assert(!inversion_list_not_equal(set_a_uint16, set_a_uint8));
+        assert(!inversion_list_not_equal(set_a_uint16, set_a_uint16));
+        assert(!inversion_list_not_equal(set_a_uint16, set_a_uint32));
+        assert(!inversion_list_not_equal(set_a_uint32, set_a_uint8));
+        assert(!inversion_list_not_equal(set_a_uint32, set_a_uint16));
+        assert(!inversion_list_not_equal(set_a_uint32, set_a_uint32));
+
+        assert(inversion_list_equal(set_a_bis_uint8, set_a_uint8));
+        assert(inversion_list_equal(set_a_bis_uint8, set_a_uint16));
+        assert(inversion_list_equal(set_a_bis_uint8, set_a_uint32));
+        assert(inversion_list_equal(set_a_bis_uint16, set_a_uint8));
+        assert(inversion_list_equal(set_a_bis_uint16, set_a_uint16));
+        assert(inversion_list_equal(set_a_bis_uint16, set_a_uint32));
+        assert(inversion_list_equal(set_a_bis_uint32, set_a_uint8));
+        assert(inversion_list_equal(set_a_bis_uint32, set_a_uint16));
+        assert(inversion_list_equal(set_a_bis_uint32, set_a_uint32));
+        assert(!inversion_list_not_equal(set_a_bis_uint8, set_a_uint8));
+        assert(!inversion_list_not_equal(set_a_bis_uint8, set_a_uint16));
+        assert(!inversion_list_not_equal(set_a_bis_uint8, set_a_uint32));
+        assert(!inversion_list_not_equal(set_a_bis_uint16, set_a_uint8));
+        assert(!inversion_list_not_equal(set_a_bis_uint16, set_a_uint16));
+        assert(!inversion_list_not_equal(set_a_bis_uint16, set_a_uint32));
+        assert(!inversion_list_not_equal(set_a_bis_uint32, set_a_uint8));
+        assert(!inversion_list_not_equal(set_a_bis_uint32, set_a_uint16));
+        assert(!inversion_list_not_equal(set_a_bis_uint32, set_a_uint32));
+
+        assert(!inversion_list_equal(set_a_uint8, set_b_uint8));
+        assert(!inversion_list_equal(set_a_uint8, set_b_uint16));
+        assert(!inversion_list_equal(set_a_uint8, set_b_uint32));
+        assert(!inversion_list_equal(set_a_uint16, set_b_uint8));
+        assert(!inversion_list_equal(set_a_uint16, set_b_uint16));
+        assert(!inversion_list_equal(set_a_uint16, set_b_uint32));
+        assert(!inversion_list_equal(set_a_uint32, set_b_uint8));
+        assert(!inversion_list_equal(set_a_uint32, set_b_uint16));
+        assert(!inversion_list_equal(set_a_uint32, set_b_uint32));
+        assert(inversion_list_not_equal(set_a_uint8, set_b_uint8));
+        assert(inversion_list_not_equal(set_a_uint8, set_b_uint16));
+        assert(inversion_list_not_equal(set_a_uint8, set_b_uint32));
+        assert(inversion_list_not_equal(set_a_uint16, set_b_uint8));
+        assert(inversion_list_not_equal(set_a_uint16, set_b_uint16));
+        assert(inversion_list_not_equal(set_a_uint16, set_b_uint32));
+        assert(inversion_list_not_equal(set_a_uint32, set_b_uint8));
+        assert(inversion_list_not_equal(set_a_uint32, set_b_uint16));
+        assert(inversion_list_not_equal(set_a_uint32, set_b_uint32));
+
+        assert(!inversion_list_equal(set_a_uint8, set_c));
+        assert(!inversion_list_equal(set_c, set_a_uint8));
+        assert(inversion_list_not_equal(set_a_uint8, set_c));
+        assert(inversion_list_not_equal(set_c, set_a_uint8));
+
+        assert(inversion_list_not_equal(set_a_uint8, set_d));
+        assert(inversion_list_not_equal(set_b_uint16, set_d));
+        assert(inversion_list_not_equal(set_c, set_d));
+
+        inversion_list_destroy(set_a_uint8);
+        inversion_list_destroy(set_a_uint16);
+        inversion_list_destroy(set_a_uint32);
+        inversion_list_destroy(set_a_bis_uint8);
+        inversion_list_destroy(set_a_bis_uint16);
+        inversion_list_destroy(set_a_bis_uint32);
+
+        inversion_list_destroy(set_b_uint8);
+        inversion_list_destroy(set_b_uint16);
+        inversion_list_destroy(set_b_uint32);
+
+        inversion_list_destroy(set_c);
+        inversion_list_destroy(set_d);
+    }
     inversion_list_finish();
     return EXIT_SUCCESS;
 }
