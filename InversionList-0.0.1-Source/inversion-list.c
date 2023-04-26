58f58da6f2fae9bf0ac3f8ddd7795f7b20980b1a
Hugo <>
2023-04-26 10:31:41 +0200

    Change code duplication to macro definition

--- InversionList-0.0.1-Source/inversion-list.c
+++ InversionList-0.0.1-Source/inversion-list.c
@@ -80,171 +80,79 @@ bool inversion_list_finish(void) {
   }
 }
 
-static int _compare_uint8(const void *value1, const void *value2) {
-  return *(uint8_t *)value1 - *(uint8_t *)value2;  // NOLINT(readability/casting)
-}
+#define DEFINE_COMPARE(type)\
+static int _compare_##type(const void *value1, const void *value2) {\
+  return *(type##_t *)value1 - *(type##_t *)value2;\
+}\
 
-static int _compare_uint16(const void *value1, const void *value2) {
-  return *(uint16_t *)value1 - *(uint16_t *)value2;  // NOLINT(readability/casting)
-}
-
-static int _compare_uint32(const void *value1, const void *value2) {
-  return *(uint32_t *)value1 - *(uint32_t *)value2;  // NOLINT(readability/casting)
-}
+DEFINE_COMPARE(uint8);
+DEFINE_COMPARE(uint16);
+DEFINE_COMPARE(uint32);
 
 /**
  * @brief Return an errno value in case of error or 0 otherwise.
  */
-static int _compute_size_and_support_uint8(unsigned int capacity, size_t count,
-                                            const void *buffer, size_t *size,
-                                            unsigned int *support) {
-  uint8_t *buffer_uint8 = (uint8_t *)buffer;
-  qsort(buffer_uint8, count, sizeof(uint8_t), _compare_uint8);
-  if (buffer_uint8[count - 1] >= capacity) {
-    return EINVAL;
-  }
-  *size = 2;
-  *support += 1;
-  for (unsigned int i = 1; i < count; i++) {
-    if (buffer_uint8[i] != buffer_uint8[i - 1]) {
-      *support += 1;
-      if (buffer_uint8[i] > buffer_uint8[i - 1] + 1) {
-        *size += 2;
-      }
-    }
-  }
-
-  return 0;
+#define DEFINE_COMPUTE_SIZE_AND_SUPPORT(type)\
+static int _compute_size_and_support_##type(unsigned int capacity, size_t count,\
+                                            type##_t *buffer, size_t *size,\
+                                            unsigned int *support) {\
+  qsort(buffer, count, sizeof(type##_t), _compare_##type);\
+  if (buffer[count - 1] >= capacity) {\
+    return EINVAL;\
+  }\
+  *size = 2;\
+  *support += 1;\
+  for (unsigned int i = 1; i < count; i++) {\
+    if (buffer[i] != buffer[i - 1]) {\
+      *support += 1;\
+      if (buffer[i] > buffer[i - 1] + 1) {\
+        *size += 2;\
+      }\
+    }\
+  }\
+\
+  return 0;\
 }
 
+DEFINE_COMPUTE_SIZE_AND_SUPPORT(uint8);
+DEFINE_COMPUTE_SIZE_AND_SUPPORT(uint16);
+DEFINE_COMPUTE_SIZE_AND_SUPPORT(uint32);
 
-/**
- * @brief Return an errno value in case of error or 0 otherwise.
- */
-static int _compute_size_and_support_uint16(unsigned int capacity, size_t count,
-                                            const void *buffer, size_t *size,
-                                            unsigned int *support) {
-  uint16_t *buffer_uint16 = (uint16_t *)buffer;
-  qsort(buffer_uint16, count, sizeof(uint16_t), _compare_uint16);
-  if (buffer_uint16[count - 1] >= capacity) {
-    return EINVAL;
-  }
-  *size = 2;
-  *support += 1;
-  for (unsigned int i = 1; i < count; i++) {
-    if (buffer_uint16[i] != buffer_uint16[i - 1]) {
-      *support += 1;
-      if (buffer_uint16[i] > buffer_uint16[i - 1] + 1) {
-        *size += 2;
-      }
-    }
-  }
-
-  return 0;
+#define DEFINE_FILL_COUPLES(type)\
+static void _fill_couples_##type(size_t count, type##_t *couples, const type##_t *buffer) {\
+  unsigned int i = 0;\
+  unsigned int current = 0;\
+\
+  couples[current] = buffer[i];\
+  current++;\
+  couples[current] = buffer[i] + 1;\
+\
+  for (i = 1; i < count; i++) {\
+    if (buffer[i] == couples[current]) {\
+      couples[current]++;\
+    } else if (buffer[i] > couples[current]) {\
+      current++;\
+      couples[current] = buffer[i];\
+      current++;\
+      couples[current] = buffer[i] + 1;\
+    }\
+  }\
 }
 
-/**
- * @brief Return an errno value in case of error or 0 otherwise.
- */
-static int _compute_size_and_support_uint32(unsigned int capacity, size_t count,
-                                            const void *buffer, size_t *size,
-                                            unsigned int *support) {
-  uint32_t *buffer_uint32 = (uint32_t *)buffer;
-  qsort(buffer_uint32, count, sizeof(uint32_t), _compare_uint32);
-  if (buffer_uint32[count - 1] >= capacity) {
-    return EINVAL;
-  }
-  *size = 2;
-  *support += 1;
-  for (unsigned int i = 1; i < count; i++) {
-    if (buffer_uint32[i] != buffer_uint32[i - 1]) {
-      *support += 1;
-      if (buffer_uint32[i] > buffer_uint32[i - 1] + 1) {
-        *size += 2;
-      }
-    }
-  }
+DEFINE_FILL_COUPLES(uint8);
+DEFINE_FILL_COUPLES(uint16);
+DEFINE_FILL_COUPLES(uint32);
 
-  return 0;
-}
+#define DEFINE_COPY_UNSIGNED_INT_ARRAY_TO_TYPE_ARRAY(type)\
+static void _copy_unsigned_int_array_to_##type##_array(size_t count, const unsigned int *src, type##_t *dst) {\
+  for (size_t i = 0; i < count; i++) {\
+    dst[i] = (type##_t)src[i];\
+  }\
+}\
 
-static void _fill_couples_uint8(size_t count, uint8_t *couples, const uint8_t *buffer) {
-  unsigned int i = 0;
-  unsigned int current = 0;
-
-  couples[current] = buffer[i];
-  current++;
-  couples[current] = buffer[i] + 1;
-
-  for (i = 1; i < count; i++) {
-    if (buffer[i] == couples[current]) {
-      couples[current]++;
-    } else if (buffer[i] > couples[current]) {
-      current++;
-      couples[current] = buffer[i];
-      current++;
-      couples[current] = buffer[i] + 1;
-    }
-  }
-}
-
-static void _fill_couples_uint16(size_t count, uint16_t *couples, const uint16_t *buffer) {
-  unsigned int i = 0;
-  unsigned int current = 0;
-
-  couples[current] = buffer[i];
-  current++;
-  couples[current] = buffer[i] + 1;
-
-  for (i = 1; i < count; i++) {
-    if (buffer[i] == couples[current]) {
-      couples[current]++;
-    } else if (buffer[i] > couples[current]) {
-      current++;
-      couples[current] = buffer[i];
-      current++;
-      couples[current] = buffer[i] + 1;
-    }
-  }
-}
-
-static void _fill_couples_uint32(size_t count, uint32_t *couples, const uint32_t *buffer) {
-  unsigned int i = 0;
-  unsigned int current = 0;
-
-  couples[current] = buffer[i];
-  current++;
-  couples[current] = buffer[i] + 1;
-
-  for (i = 1; i < count; i++) {
-    if (buffer[i] == couples[current]) {
-      couples[current]++;
-    } else if (buffer[i] > couples[current]) {
-      current++;
-      couples[current] = buffer[i];
-      current++;
-      couples[current] = buffer[i] + 1;
-    }
-  }
-}
-
-static void _copy_unsigned_int_array_to_uint8_array(size_t count, const unsigned int *src, uint8_t *dst) {
-  for (size_t i = 0; i < count; i++) {
-    dst[i] = (uint8_t)src[i];
-  }
-}
-
-static void _copy_unsigned_int_array_to_uint16_array(size_t count, const unsigned int *src, uint16_t *dst) {
-  for (size_t i = 0; i < count; i++) {
-    dst[i] = (uint16_t)src[i];
-  }
-}
-
-static void _copy_unsigned_int_array_to_uint32_array(size_t count, const unsigned int *src, uint32_t *dst) {
-  for (size_t i = 0; i < count; i++) {
-    dst[i] = (uint32_t)src[i];
-  }
-}
+DEFINE_COPY_UNSIGNED_INT_ARRAY_TO_TYPE_ARRAY(uint8);
+DEFINE_COPY_UNSIGNED_INT_ARRAY_TO_TYPE_ARRAY(uint16);
+DEFINE_COPY_UNSIGNED_INT_ARRAY_TO_TYPE_ARRAY(uint32);
 
 InversionList *inversion_list_create(unsigned int capacity, size_t count, const unsigned int *values) {
   assert(_counter > 0);
@@ -280,11 +188,11 @@ InversionList *inversion_list_create(unsigned int capacity, size_t count, const 
   if (count) {
     int status;
     if (can_use_8_bits) {
-      status = _compute_size_and_support_uint8(capacity, count, buffer, &size, &support);
+      status = _compute_size_and_support_uint8(capacity, count, (uint8_t *)buffer, &size, &support);
     } else if (can_use_16_bits) {
-      status = _compute_size_and_support_uint16(capacity, count, buffer, &size, &support);
+      status = _compute_size_and_support_uint16(capacity, count, (uint16_t *)buffer, &size, &support);
     } else {
-      status = _compute_size_and_support_uint32(capacity, count, buffer, &size, &support);
+      status = _compute_size_and_support_uint32(capacity, count, (uint32_t *)buffer, &size, &support);
     }
 
     if (status != 0) {
@@ -440,77 +348,34 @@ InversionList *inversion_list_clone(const InversionList *set) {
   }
 }
 
-static void _compute_complement_uint8(const InversionList *source,
-                                      InversionList *complement,
-                                      size_t final_size) {
-  uint8_t *source_couples = source->couples.uint8;
-  uint8_t *complement_couples = complement->couples.uint8;
-                                        
-  if (source_couples[0] == 0) {
-    if (source_couples[source->size - 1] == source->capacity) {
-      memcpy(complement_couples, source_couples + 1, final_size * sizeof(uint8_t));  // flawfinder: ignore
-    } else {
-      memcpy(complement_couples, source_couples + 1, (final_size - 1) * sizeof(uint8_t));  // flawfinder: ignore
-      complement_couples[final_size - 1] = source->capacity;
-    }
-  } else {
-    complement_couples[0] = 0;
-    if (source_couples[source->size - 1] == source->capacity) {
-      memcpy(complement_couples + 1, source_couples, (final_size - 1) * sizeof(uint8_t));  // flawfinder: ignore
-    } else {
-      memcpy(complement_couples + 1, source_couples, (final_size - 2) * sizeof(uint8_t));  // flawfinder: ignore
-      complement_couples[final_size - 1] = source->capacity;
-    }
-  }
+#define DEFINE_COMPUTE_COMPLEMENT(type)\
+static void _compute_complement_##type(const InversionList *source,\
+                                      InversionList *complement,\
+                                      size_t final_size) {\
+  type##_t *source_couples = source->couples.type;\
+  type##_t *complement_couples = complement->couples.type;\
+\
+  if (source_couples[0] == 0) {\
+    if (source_couples[source->size - 1] == source->capacity) {\
+      memcpy(complement_couples, source_couples + 1, final_size * sizeof(type##_t));\
+    } else {\
+      memcpy(complement_couples, source_couples + 1, (final_size - 1) * sizeof(type##_t));\
+      complement_couples[final_size - 1] = source->capacity;\
+    }\
+  } else {\
+    complement_couples[0] = 0;\
+    if (source_couples[source->size - 1] == source->capacity) {\
+      memcpy(complement_couples + 1, source_couples, (final_size - 1) * sizeof(type##_t));\
+    } else {\
+      memcpy(complement_couples + 1, source_couples, (final_size - 2) * sizeof(type##_t));\
+      complement_couples[final_size - 1] = source->capacity;\
+    }\
+  }\
 }
 
-static void _compute_complement_uint16(const InversionList *source,
-                                      InversionList *complement,
-                                      size_t final_size) {
-  uint16_t *source_couples = source->couples.uint16;
-  uint16_t *complement_couples = complement->couples.uint16;
-                                        
-  if (source_couples[0] == 0) {
-    if (source_couples[source->size - 1] == source->capacity) {
-      memcpy(complement_couples, source_couples + 1, final_size * sizeof(uint16_t));  // flawfinder: ignore
-    } else {
-      memcpy(complement_couples, source_couples + 1, (final_size - 1) * sizeof(uint16_t));  // flawfinder: ignore
-      complement_couples[final_size - 1] = source->capacity;
-    }
-  } else {
-    complement_couples[0] = 0;
-    if (source_couples[source->size - 1] == source->capacity) {
-      memcpy(complement_couples + 1, source_couples, (final_size - 1) * sizeof(uint16_t));  // flawfinder: ignore
-    } else {
-      memcpy(complement_couples + 1, source_couples, (final_size - 2) * sizeof(uint16_t));  // flawfinder: ignore
-      complement_couples[final_size - 1] = source->capacity;
-    }
-  }
-}
-
-static void _compute_complement_uint32(const InversionList *source,
-                                      InversionList *complement,
-                                      size_t final_size) {
-  uint32_t *source_couples = source->couples.uint32;
-  uint32_t *complement_couples = complement->couples.uint32;
-                                        
-  if (source_couples[0] == 0) {
-    if (source_couples[source->size - 1] == source->capacity) {
-      memcpy(complement_couples, source_couples + 1, final_size * sizeof(uint32_t));  // flawfinder: ignore
-    } else {
-      memcpy(complement_couples, source_couples + 1, (final_size - 1) * sizeof(uint32_t));  // flawfinder: ignore
-      complement_couples[final_size - 1] = source->capacity;
-    }
-  } else {
-    complement_couples[0] = 0;
-    if (source_couples[source->size - 1] == source->capacity) {
-      memcpy(complement_couples + 1, source_couples, (final_size - 1) * sizeof(uint32_t));  // flawfinder: ignore
-    } else {
-      memcpy(complement_couples + 1, source_couples, (final_size - 2) * sizeof(uint32_t));  // flawfinder: ignore
-      complement_couples[final_size - 1] = source->capacity;
-    }
-  }
-}
+DEFINE_COMPUTE_COMPLEMENT(uint8);
+DEFINE_COMPUTE_COMPLEMENT(uint16);
+DEFINE_COMPUTE_COMPLEMENT(uint32);
 
 InversionList *inversion_list_complement(const InversionList *set) {
   assert(_counter > 0);
@@ -589,74 +454,31 @@ static bool _add_string(char **pstring, unsigned int *plength, const char *add) 
 /**
  * @brief Return true if an error occurred, false otherwise.
  */
-static bool _add_uint8_string(const uint8_t *couples, unsigned int index, bool *first,
-                       char **string, unsigned int *length) {
-  unsigned int value;
-  for (value = couples[index]; value < couples[index + 1]; value++) {
-    if (*first) {
-      *first = false;
-    } else {
-      if (!_add_string(string, length, ", ")) {
-        return true;
-      }
-    }
-    static char current[100];  // flawfinder: ignore
-    snprintf(current, sizeof current, "%u", value);
-    if (!_add_string(string, length, current)) {
-      return true;
-    }
-  }
-
-  return false;
+#define DEFINE_ADD_TYPE_STRING(type)\
+static bool _add_##type##_string(const type##_t *couples, unsigned int index, bool *first,\
+                       char **string, unsigned int *length) {\
+  unsigned int value;\
+  for (value = couples[index]; value < couples[index + 1]; value++) {\
+    if (*first) {\
+      *first = false;\
+    } else {\
+      if (!_add_string(string, length, ", ")) {\
+        return true;\
+      }\
+    }\
+    static char current[100];\
+    snprintf(current, sizeof current, "%u", value);\
+    if (!_add_string(string, length, current)) {\
+      return true;\
+    }\
+  }\
+\
+  return false;\
 }
 
-/**
- * @brief Return true if an error occurred, false otherwise.
- */
-static bool _add_uint16_string(const uint16_t *couples, unsigned int index, bool *first,
-                       char **string, unsigned int *length) {
-  unsigned int value;
-  for (value = couples[index]; value < couples[index + 1]; value++) {
-    if (*first) {
-      *first = false;
-    } else {
-      if (!_add_string(string, length, ", ")) {
-        return true;
-      }
-    }
-    static char current[100];  // flawfinder: ignore
-    snprintf(current, sizeof current, "%u", value);
-    if (!_add_string(string, length, current)) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-/**
- * @brief Return true if an error occurred, false otherwise.
- */
-static bool _add_uint32_string(const uint32_t *couples, unsigned int index, bool *first,
-                       char **string, unsigned int *length) {
-  unsigned int value;
-  for (value = couples[index]; value < couples[index + 1]; value++) {
-    if (*first) {
-      *first = false;
-    } else {
-      if (!_add_string(string, length, ", ")) {
-        return true;
-      }
-    }
-    static char current[100];  // flawfinder: ignore
-    snprintf(current, sizeof current, "%u", value);
-    if (!_add_string(string, length, current)) {
-      return true;
-    }
-  }
-
-  return false;
-}
+DEFINE_ADD_TYPE_STRING(uint8);
+DEFINE_ADD_TYPE_STRING(uint16);
+DEFINE_ADD_TYPE_STRING(uint32);
 
 const char *inversion_list_to_string(const InversionList *set) {
   static char *string = NULL;
